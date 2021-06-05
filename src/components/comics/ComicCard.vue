06eb4d067c90ae244c648068bb6c575b0fc4aa8d
Juan Pablo Olaya <juanpa-1920@hotmail.com>
2021-06-04 23:15:20 -0500

    Descomponiendo la vista ComicDetail

--- src/components/comics/ComicCard.vue
+++ src/components/comics/ComicCard.vue
@@ -19,59 +19,30 @@
       </div>
     </div>
     <footer class="card-footer">
-      <div class="field has-addons is-flex-grow-1">
-        <p class="control is-flex-grow-1 is-flex">
-          <button
-            class="button is-flex-grow-1"
-            :disabled="actualComic.num <= 1"
-            @click="getPreviousComic()"
-          >
-            Previous
-          </button>
-        </p>
-        <p class="control is-flex-grow-1 is-flex">
-          <button class="button is-flex-grow-1" @click="getRandomComic()">
-            Random
-          </button>
-        </p>
-        <p class="control is-flex-grow-1 is-flex">
-          <button
-            class="button is-flex-grow-1"
-            :disabled="actualComic.num >= latestNum"
-            @click="getNextComic()"
-          >
-            Next
-          </button>
-        </p>
-      </div>
+      <comic-actions />
     </footer>
   </div>
 </template>
 <script>
 import { mapState, mapActions } from 'vuex';
 import types from '@/store/modules/comics/types';
+import ComicActions from './ComicActions.vue';
 
 export default {
   name: 'ComicCard',
   computed: {
     ...mapState(types.PATH, ['actualComic', 'latestNum']),
   },
+  components: {
+    ComicActions,
+  },
   methods: {
     ...mapActions(types.PATH, {
       getComic: types.actions.GET_COMIC,
       getLatestNum: types.actions.GET_LATEST_NUM,
     }),
-    getRandomComic() {
-      this.getComic(this.getRandomNum());
-    },
     getRandomNum() {
       return Math.floor(Math.random() * (this.latestNum - 1 + 1) + 1);
-    },
-    getPreviousComic() {
-      this.getComic(this.actualComic.num - 1);
-    },
-    getNextComic() {
-      this.getComic(this.actualComic.num + 1);
     },
   },
   mounted() {
